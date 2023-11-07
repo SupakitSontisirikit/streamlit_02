86cfdc6d07542bce6ff312f21f85c89a4efccf63
Supakit <66056078@kmitl.ac.th>
2023-11-08 02:10:10 +0700

    Create tab and column

--- app.py
+++ app.py
@@ -1,26 +1,49 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+st.set_page_config(layout='wide')
+st.markdown('สวัสดี! *Streamlit*')
+st.title('Layout and Decoration')
 st.write("""
   เราจะลองทำ San Francisco Dataset กันดู
 """)
-col1,col2,col3 = st.columns(3)
-with col1:
-    st.write('Column1')
-with col2:
-    st.write('Column2')
-with col3:
-    st.write('Column3')
-
 df = pd.read_csv('trees.csv')
-
 df2 = pd.DataFrame(
     df.groupby(['dbh']).count()['tree_id']
 )
-
 df2.columns = ['tree_count']
 
-st.line_chart(df2)
-st.caption('กราฟแสดงจำนวนต้นไม้')
+st.divider()
+#
+# col1,col2,col3 = st.columns(3)
+# with col1:
+#     st.write('Column1')
+#     st.line_chart(df2)
+# with col2:
+#     st.write('Column2')
+#     st.bar_chart(df2)
+# with col3:
+#     st.write('Column3')
+#     st.area_chart(df2)
+#
+# st.divider()
+
+col1,col2,col3 = st.tabs(["Line", "Bar", "Area"])
+with col1:
+    tab1,tab2,tab3 = st.tabs(["Line", "Bar", "Area"])
+with tab1:
+    st.write('Column1')
+    st.line_chart(df2)
+with col2:
+    with tab2:
+        st.write('Column2')
+        st.bar_chart(df2)
+with col3:
+    with tab3:
+        st.write('Column3')
+        st.area_chart(df2)
+
+st.caption('กราฟแสดงจำนวนต้นไม้ จัดกลุ่มตามเส้นผ่าศูนย์กลาง')
 st.title('แปรผล')
-
-
-
-print(df2)
+st.write('ส่วนใหญ่ของต้นไม้ใน SF มีเส้นผ่าศูนย์กลาง 3\' (2,721 ต้น)')
